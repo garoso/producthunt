6e8f0ecb2b169d2f7a6d769452f89d541820f1ea
garoso <david.fd.251@gmail.com>
2018-09-28 19:24:49 -0500

    Create post/products route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-  get '/products', to: 'products#index'
-  get '/products/new' => 'products#new'
+  get '/products' => 'products#index'
+  get '/products/new' => 'products#new', as: 'new_product'
+  post '/products' => 'products#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
