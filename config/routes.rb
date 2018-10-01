1d7da0e5e6ba06c6b5312eae98f87d4ae2c00af8
garoso <david.fd.251@gmail.com>
2018-10-01 11:13:20 -0500

    Adds link to edit product

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get '/products/new' => 'products#new', as: 'new_product'
   post '/products' => 'products#create'
   get '/products/:id' => 'products#show', as: 'product'
-  get 'products/:id/edit' =>'products#edit'
+  get 'products/:id/edit' =>'products#edit', as: 'edit_product'
   patch 'products/:id' => 'products#update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
