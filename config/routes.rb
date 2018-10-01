e5f314dd7c46490d8b7c1c975919aa62e0c284e3
garoso <david.fd.251@gmail.com>
2018-10-01 10:57:52 -0500

    Create /products/:id/edit route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get '/products' => 'products#index'
   get '/products/new' => 'products#new', as: 'new_product'
   post '/products' => 'products#create'
-  get '/products/:id' => 'products#show'
+  get '/products/:id' => 'products#show', as: 'product' 
+  get 'products/:id/edit' =>'products#edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
