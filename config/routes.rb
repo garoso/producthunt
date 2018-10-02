f1a12d46037a94590d084c053088f5aeeb49e785
garoso <david.fd.251@gmail.com>
2018-10-01 21:24:24 -0500

    Create Logout funcionality

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
   
   resources :products
   resources :users, only: [:new, :create]
